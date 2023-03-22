a7b50d3232daaeea7a4b0dcdbe231033c71bb7b7
VladimirZelenokor1 <119796161+VladimirZelenokor1@users.noreply.github.com>
2023-03-22 10:23:20 +0000

    Deploying to gh-pages from @ VladimirZelenokor1/My-Website@4c62d2bf9ea15d9be42b20e1f57c9e41c3d8a680 🚀

---
+++ _app/immutable/chunks/1.ded240d2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.40c8c88b.js";export{t as component};
---
+++ _app/immutable/chunks/2.864929e7.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page.da46b06b.js";import{default as t}from"../entry/_page.svelte.e75f0d9e.js";export{t as component,r as universal};
